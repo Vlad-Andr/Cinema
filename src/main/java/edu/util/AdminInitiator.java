d466140449c17302828bdffe337a0f36ade7172e
Vlad-Andr <andreev.v99@gmail.com>
2021-06-29 21:42:46 +0300

    added admin initialization

---
+++ src/main/java/edu/util/AdminInitiator.java
@@ -0,0 +1,33 @@
+package edu.util;
+
+import edu.model.Role;
+import edu.model.RoleName;
+import edu.model.User;
+import edu.service.RoleService;
+import edu.service.UserService;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import java.util.Set;
+
+@Component
+public class AdminInitiator {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public AdminInitiator(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        roleService.add(new Role(RoleName.ROLE_ADMIN));
+        roleService.add(new Role(RoleName.ROLE_USER));
+
+        User alice = new User();
+        alice.setEmail("admin");
+        alice.setPassword("admin");
+        alice.setRoles(Set.of(roleService.getRoleByName(RoleName.ROLE_ADMIN)));
+        userService.add(alice);
+    }
+}
